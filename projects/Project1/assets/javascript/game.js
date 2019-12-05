4a8d81a2bd92045d945abfe54f834537780b9a0b
Nolan Hewitt <nolanhewitt@aol.com>
2019-12-04 21:40:18 -0600

    improved first and second games

--- projects/Project1/assets/javascript/game.js
+++ projects/Project1/assets/javascript/game.js
@@ -4,44 +4,45 @@ let guesses = 5;
 let wins = 0;
 let losses = 0;
 let allchoices = "";
+let userGuess;
 //Array of possible computer choices and random choice for game.
 let computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess);
 //User input controls
 // This function is run whenever the user presses a key.
  document.onkeyup = function(event) {
 // Determines which key was pressed.
-    let userGuess = event.key.toLowerCase();
-    console.log(userGuess);
+    userGuess = event.key.toLowerCase();
 
 //Chooses a new letter for the computer every time the player inputs a key
 
 //Win Conditions
     if (userGuess === computerGuess) {
         wins++;
-        guesses=6;
+        guesses=5;
         document.getElementById("win").innerHTML = "Wins: " + wins;
         allchoices = "";
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerGuess);
+        document.getElementById("guesses").innerHTML ="Guesses Left: " + guesses;
+        document.getElementById("guess1").innerHTML = "Guess: " + allchoices;
     }
 
 //Write the letter that was guessed if it's incorrect
     else {
         guesses--;
+        allchoices = allchoices+userGuess+", ";
         document.getElementById("guesses").innerHTML ="Guesses Left: " + guesses;
         document.getElementById("guess1").innerHTML = "Guess: " + allchoices;
-        allchoices = allchoices+userGuess+", ";
     }
 
     //Lose Condition
     if (guesses === 0) {
-        guesses=6;
+        guesses=5;
         losses++;
         document.getElementById("lose").innerHTML = "Losses: " + losses;
         allchoices = "";
+        document.getElementById("guesses").innerHTML ="Guesses Left: " + guesses;
+        document.getElementById("guess1").innerHTML = "Guess: " + allchoices;
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerGuess);
     }
 }
--- projects/Project2/assets/javascript/game.js
+++ projects/Project2/assets/javascript/game.js
@@ -12,21 +12,35 @@ let wins = 0;
 //Losses Variable
 let losses = 0;
 
-//Crystal values (Between 3 and 9 for each crystal)
-let rng1 = Math.floor(Math.random() * 7)+3;
-let rng2 = Math.floor(Math.random() * 7)+3;
-let rng3 = Math.floor(Math.random() * 7)+3;
-let rng4 = Math.floor(Math.random() * 7)+3;
+//Array for rng numbers
+let random = [];
 
-//Prevent duplicate rng values for crystals
-//?
+//Prevent duplicate rng values for crystals using a number generator
+function generateRan(){
+  const max = 6;
+  for(var i = 0;i<max ; i++){
+      var temp = Math.floor(Math.random()*max+4);
+      if(random.indexOf(temp) == -1){
+          random.push(temp);
+      }
+      else
+       i--;
+  }
+};
+generateRan();
 
+//Crystal values (Between 5 and 9 for each crystal)
+let rng1 = random[0];
+let rng2 = random[1];
+let rng3 = random[2];
+let rng4 = random[3];
 
 //crystal1 click command
   const one = document.getElementById("crystal1");
   one.addEventListener("click", function() {
   score=score+rng1;
   document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("condition").innerHTML = "Each crystal gives points when clicked!";
 
     if (score === winningnumber){
         document.getElementById("condition").innerHTML = "You Won!";
@@ -54,20 +68,22 @@ let rng4 = Math.floor(Math.random() * 7)+3;
         rng4 = Math.floor(Math.random() * 7)+3;
         winningnumber = Math.floor(Math.random() * 51)+50;
         document.getElementById("goal").innerHTML = "Get " + winningnumber +" points to win!";
-                                   }
-})
+                                   };
+});
 
 //crystal2 click command
   const two = document.getElementById("crystal2");
   two.addEventListener("click", function() {
   score=score+rng2;
   document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("condition").innerHTML = "Each crystal gives points when clicked!";
 
     if (score === winningnumber){
         document.getElementById("condition").innerHTML = "You Won!";
         wins = wins+1;
         document.getElementById("wins").innerHTML = "Wins: " + wins;
         score=0;
+        document.getElementById("score").innerHTML = "Score: " + score;
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
@@ -81,25 +97,29 @@ let rng4 = Math.floor(Math.random() * 7)+3;
         losses = losses+1;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
         score=0;
+        document.getElementById("score").innerHTML = "Score: 0";
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
         rng4 = Math.floor(Math.random() * 7)+3;
         winningnumber = Math.floor(Math.random() * 51)+50;
         document.getElementById("goal").innerHTML = "Get " + winningnumber +" points to win!";
-                                   }
-})
+                                   };
+});
 
 //crystal3 click command
   const three = document.getElementById("crystal3");
   three.addEventListener("click", function() {
   score=score+rng3;
   document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("condition").innerHTML = "Each crystal gives points when clicked!";
+
     if (score === winningnumber){
         document.getElementById("condition").innerHTML = "You Won!";
         wins = wins+1;
         document.getElementById("wins").innerHTML = "Wins: " + wins;
         score=0;
+        document.getElementById("score").innerHTML = "Score: " + score;
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
@@ -113,25 +133,29 @@ let rng4 = Math.floor(Math.random() * 7)+3;
         losses = losses+1;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
         score=0;
+        document.getElementById("score").innerHTML = "Score: 0";
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
         rng4 = Math.floor(Math.random() * 7)+3;
         winningnumber = Math.floor(Math.random() * 51)+50;
         document.getElementById("goal").innerHTML = "Get " + winningnumber +" points to win!";
-                                   }
-})
+                                   };
+});
 
 //crystal4 click command
   const four = document.getElementById("crystal4");
   four.addEventListener("click", function() {
   score=score+rng4;
   document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("condition").innerHTML = "Each crystal gives points when clicked!";
+
     if (score === winningnumber){
         document.getElementById("condition").innerHTML = "You Won!";
         wins = wins+1;
         document.getElementById("wins").innerHTML = "Wins: " + wins;
         score=0;
+        document.getElementById("score").innerHTML = "Score: " + score;
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
@@ -145,11 +169,12 @@ let rng4 = Math.floor(Math.random() * 7)+3;
         losses = losses+1;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
         score=0;
+        document.getElementById("score").innerHTML = "Score: 0";
         rng1 = Math.floor(Math.random() * 7)+3;
         rng2 = Math.floor(Math.random() * 7)+3;
         rng3 = Math.floor(Math.random() * 7)+3;
         rng4 = Math.floor(Math.random() * 7)+3;
         winningnumber = Math.floor(Math.random() * 51)+50;
         document.getElementById("goal").innerHTML = "Get " + winningnumber +" points to win!";
-                                   }
-})
+                                   };
+});
